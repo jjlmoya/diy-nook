650dcc8c78dc820fd6f2bc2c2ee8531a8ad1fb02
jjlmoya <jj.lopez@avoristravel.com>
2020-04-22 19:28:38 +0200

    Remove bug files

--- src/data/ac/category.data.js
+++
@@ -1,9 +0,0 @@
-import CLOTH from './crafts/categories/cloth.category'
-import FURNITURE from './crafts/categories/furniture.category'
-import OBJECT from './crafts/categories/object.category'
-
-export default {
-  ...CLOTH,
-  ...FURNITURE,
-  ...OBJECT
-}
